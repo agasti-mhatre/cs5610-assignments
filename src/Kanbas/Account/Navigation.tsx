f9d35426a311bd8dbf7d8498e4561601bf4b7c32
agasti-mhatre <agastimhatre6@gmail.com>
2024-10-03 19:25:14 -0400

    Finished

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kanbas/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/>
+      <Link to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0 me-3"> Signin </Link> <br/>
+      <Link to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0 me-3"> Signup </Link> <br/>
+      <Link to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0 me-3"> Profile </Link> <br/>
     </div>
 );}
--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -4,10 +4,12 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" /><br/>
-      <input placeholder="password" type="password" /><br/>
-      <input placeholder="verify password" type="password" /><br/>
-      <Link to="/Kanbas/Account/Profile" > Sign up </Link><br />
-      <Link to="/Kanbas/Account/Signin" >Sign in</Link>
+      <div className="d-flex flex-column">
+        <input placeholder="username" className="form-control mb-1"/>
+        <input placeholder="password" type="password" className="form-control mb-1"/>
+        <input placeholder="verify password" type="password" className="form-control"/>
+        <Link to="/Kanbas/Account/Profile" className="btn btn-primary w-100 mt-3"> Sign up </Link>
+        <Link to="/Kanbas/Account/Signin" className="btn btn-secondary w-100 mt-3">Sign in</Link>
+      </div>
     </div>
 );}

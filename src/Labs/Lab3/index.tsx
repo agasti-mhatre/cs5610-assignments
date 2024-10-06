8afa7516f97e865a0d965064ba42a87253d16145
agasti-mhatre <agastimhatre6@gmail.com>
2024-10-06 17:45:25 -0400

    up to 2.5

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -4,15 +4,25 @@ import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
 import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -38,6 +48,16 @@ export default function Lab3() {
         <ForLoops />
         <MapFunction />
         <FindFunction />
+        <FindIndex />
+        <FilterFunction />
+        <JsonStringify />
+        <House />
+        <TodoItem />
+        <TodoList />
+        <Spreading />
+        <Destructing />
+        <FunctionDestructing />
+        <DestructingImports />
       </div>
     );
 }

2e0b60b61eaa11d3b5b65dda2b818f7bd6a242a0
agasti-mhatre <agastimhatre6@gmail.com>
2024-10-12 19:05:19 -0400

    Up to section 3.4

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,144 +1,32 @@
 import { Link } from "react-router-dom";
-
+import { courses } from "./Database";
 export default function Dashboard() {
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
-
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/react.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS1234 React JS
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Full Stack software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+              <div className="card rounded-3 overflow-hidden">
+                <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                      className="wd-dashboard-course-link text-decoration-none text-dark" >
+                  <img src={course.photo} width="100%" height={160} />
+                  <div className="card-body">
+                    <h5 className="wd-dashboard-course-title card-title">
+                      {course.name} </h5>
+                    <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                      {course.description} </p>
+                    <button className="btn btn-primary"> Go </button>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/dart.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS1000 Dart
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Dart software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/flutter.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS2000 Flutter
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Flutter software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/java.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS3000 Java
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Java software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/python.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS4000 Python
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Python software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/spring.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS5000 Spring
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      Spring software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/sql.jpg" width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                    CS5000 SQL
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                      SQL software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-
+          ))}
         </div>
-
       </div>
     </div>
-
   );
 }

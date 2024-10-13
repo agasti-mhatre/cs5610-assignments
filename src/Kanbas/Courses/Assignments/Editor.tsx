af9e583c6a836cd37f10b473c7d275d4d74d8802
agasti-mhatre <agastimhatre6@gmail.com>
2024-10-13 17:07:30 -0400

    Finished assignment 3

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { assignments, courses } from "../../Database";
+import { assignments } from "../../Database";
 import { Link } from "react-router-dom";
 
 export default function AssignmentEditor() {
